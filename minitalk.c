999f24373381157815a88508e1ee88518f310b86
Ricardo Moraes <ric.costamoraes@gmail.com>
2025-11-26 23:18:22 -0300

    convert ok

--- minitalk.c
+++ minitalk.c
@@ -3,34 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rida-cos <rida-cos@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rdcm <rdcm@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 22:04:23 by rdcm              #+#    #+#             */
-/*   Updated: 2025/11/25 21:13:28 by rida-cos         ###   ########.fr       */
+/*   Updated: 2025/11/26 23:15:35 by rdcm             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-
-int	expo(int position)
-{
-	if (expo == 0)
-		return (128);
-	else if (expo == 1)
-		return (64);
-	else if (expo == 2)
-		return (32);
-	else if (expo == 3)
-		return (16);
-	else if (expo == 4)
-		return (8);
-	else if (expo == 5)
-		return (4);
-	else if (expo == 6)
-		return (2);
-	else if (expo == 7)
-		return (0);
-}
 
 void	convert_to_binary(int number, int *b)
 {
@@ -45,16 +25,21 @@ void	convert_to_binary(int number, int *b)
 	}
 }
 
-void	convert_to_integer(int number, int	*b)
+int	convert_to_integer(int	*b)
 {
 	int	i;
+	int j;
 	int	nb;
-
+	
 	i = 0;
+	j = 128; //1 2 4 8 16 32 64 128
+	nb = 0;
 	while (i < 8)
 	{
-		nb = (expo(i)) * b[i] + nb;
+		nb = j * b[i] + nb;
+		//printf("j:%d e i:%d e nb:%d\n", j, i, nb);
 		i++;
+		j = j / 2;
 	}
 	return (nb);
 }
@@ -74,23 +59,50 @@ void	*ft_memset(void *s, int c, size_t n)
 	return (s);
 }
 
-int	main()
+int     ft_atoi(const char *str)
+{
+        int     sign;
+        int     nb;
+        int     i;
+
+        i = 0;
+        sign = 1;
+        nb = 0;
+        while (str[i] == ' ' || ((str[i] >= 9) && (str[i] <= 13)))
+                i++;
+        if ((str[i] == '-') || (str[i] == '+'))
+        {
+                if (str[i] == '-')
+                        sign = -1;
+                i++;
+                if (str[i] == '-' || str[i] == '+')
+                        return (0);
+        }
+        while ((str[i] >= '0') && (str[i] <= '9'))
+        {
+                nb = (nb * 10) + (str[i] - '0');
+                i++;
+        }
+        return (nb * sign);
+}
+
+int	main(int argc, char **argv)
 {
 	int	nb;
 	int	b[8];
 	int	i = 7;
 
-	nb = 5;
+	nb = ft_atoi(argv[1]);
 	ft_memset(b, 0, sizeof(b));
 	convert_to_binary(nb, b);
-
 	printf("\n\n");
-
 	int j = 0;
 	while (j < 8)
 	{
-		printf("%d\n", b[j]);
+		printf("%d", b[j]);
 		j++;
 	}
+	printf("\n");
+	printf("\nValor: %d\n", convert_to_integer(b));
 	return (0);
 }
